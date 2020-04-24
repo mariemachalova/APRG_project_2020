361073803e2ffa77a44d1fc878a35269212f6b1b
svestka <emikbvb@seznam.cz>
2020-04-24 12:24:13 +0200

    nacteni_dat

--- nacteni_dat.py
+++ nacteni_dat.py
@@ -1,1 +1,20 @@
+x = []
+y = []
+robot_position= []
+    self.x = x
+    self.y = y
 
+
+def get_x(self):
+    return self.x
+ def get_y(self):
+    return self.y
+
+def read_input_file(file):
+    start_position = []
+    with open(file, 'r') as f:
+        for line in f:
+            x, y = ''.join(line.split()).split(',')
+            start_position.append(Point(robot_position(x), robot_position(y)))
+
+    return start_position
