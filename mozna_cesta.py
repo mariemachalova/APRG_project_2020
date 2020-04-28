ea594366287c508756a85c26d9b665961bfea5b5
klara <xheles05@vutbr.cz>
2020-04-28 22:09:46 +0200

    nastrel

--- mozna_cesta.py
+++ mozna_cesta.py
@@ -4,7 +4,24 @@ koncovy_bod = (50, 50)
 # nase_usecka = n_u
 n_u = (zacatecni_bod[0], zacatecni_bod[1], koncovy_bod[0], koncovy_bod[1])
 # polygon_usecka
+# je potreba vyzkouset se vsemi useckami z konvexni obalky - objekt 1 nejnizsi x (seradit si x) dokud se nevrati ke start pointu - append a pop?
 p_u = (20, 50, 45, 20)
+
+
+def vytvor_usecky(polygony):
+    for polygon in polygony:
+        usecky = []
+        posledni_souradnice = len(polygon)
+        for point in polygon:
+            if point == posledni_souradnice:
+                # point[0].append ???
+                usecky.append
+            else
+                point
+
+
+
+
 
 # pod class?
 def najdi_prusecik(n_u, p_u):
@@ -14,7 +31,9 @@ def najdi_prusecik(n_u, p_u):
     y_help1 = x * (n_u[3] - n_u[1]) + n_u[1] * (n_u[2] - n_u[0]) - n_u[0] * (n_u[3] - n_u[1])
     y_help2 = (n_u[2] - n_u[0])
     y = y_help1 / y_help2
-    print(x, y)
-    if n_u[0] <= x <= n_u[2] and p_u[0] <= x <= p_u[2] and n_u[1] <= y <= n_u[3] and p_u[1] <= y <= p_u[3]:
-    return x, y
+    # print(x, y)
+    if (n_u[0] <= x <= n_u[2]) or (p_u[0] <= x <= p_u[2]) and (n_u[1] <= y <= n_u[3]) or (p_u[1] <= y <= p_u[3]):
+        print(x, y)
+        return x, y
+
 najdi_prusecik(n_u, p_u)
