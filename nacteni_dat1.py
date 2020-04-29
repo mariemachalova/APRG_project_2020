0687972a5e40fc64ca6acec6fdce55a2c9d22e6f
svestka <emikbvb@seznam.cz>
2020-04-29 12:55:49 +0200

    nacteni_dat

---
+++ nacteni_dat1.py
@@ -0,0 +1,14 @@
+pocatecni_bod = []
+koncovy_bod = []
+prekazky = []
+
+print('Startovní pozice')
+print(pocatecni_bod)
+
+def read_input_file(file):
+    with open(file, 'map_data_0.json') as f:
+        pocatecni_bod.append((prekazky(x), prekazky(y))
+            koncovy_bod = pocatecni_bod + prekazky
+
+print('Cílová pozice')
+print(koncovy_bod)
