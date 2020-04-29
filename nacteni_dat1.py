0993f3f1a2caaee43506fb4f79175573fbda882b
svestka <emikbvb@seznam.cz>
2020-04-29 13:45:55 +0200

    nacteni_dat

--- nacteni_dat1.py
+++ nacteni_dat1.py
@@ -5,8 +5,15 @@ prekazky = ()
 print('Startovní pozice')
 print(pocatecni_bod)
 
+def nacteni_mapy(prekazky):
+    with open('konvexni_obaly.py') as soubor:
+        for graham_scan in soubor
+            print(prekazky)
+
+
+
 def read_input_file(file):
-    with open(file, 'map_data_0.json') as f:
+    with open(file, 'map_data_0.json') as file:
         pocatecni_bod.append((prekazky(x), prekazky(y))
             koncovy_bod = pocatecni_bod + prekazky
 
