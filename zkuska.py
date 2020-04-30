738453a601b9c4b9cf255092f55fc3fd241299e3
klara <xheles05@vutbr.cz>
2020-04-30 14:47:14 +0200

    jestli tohle nejak vyjde...

--- zkuska.py
+++ zkuska.py
@@ -1,4 +1,3 @@
-
 zacatecni_bod = (17, 15)
 koncovy_bod = (50, 75)
 # nase_usecka = n_u
@@ -9,8 +8,6 @@ n_u = (zacatecni_bod[0], zacatecni_bod[1], koncovy_bod[0], koncovy_bod[1])
 objekty = [[(14, 46), (7.7, 77), (18.7, 89.4), (36.6, 80.3), (32.8, 54.9), (22.3, 59.9)], [(51.7, 52.0), (54.4, 22.8), (43.8, 7.9), (34.7, 10.2), (32.2, 17.4), (23.2, 11.1), (15.6, 17.4), (22.2, 42.3), (37.1, 27.2), (44.4, 30.4), (41.6, 37.4)]
 , [(63.4, 45.1), (88.0, 54.9), (72.6, 20.6), (65.7, 27.7), (63.4, 45.1)], [(47.3, 81.2), (58.5, 77.4), (62.8, 86.3), (88.2, 86.3), (71.1, 60.5), (66.2, 75.2), (51.7, 68.8)]]
 
-# c = -((n_u[3] - n_u[1]) * n_u[0] + (n_u[0] - n_u[2]) * n_u[1])
-# print(c)
 
 # n_u[0] = bodA.x
 # n_u[1] = bodA.y
@@ -22,14 +19,15 @@ objekty = [[(14, 46), (7.7, 77), (18.7, 89.4), (36.6, 80.3), (32.8, 54.9), (22.3
 # p_u[3] = bodD.y
 
 
-def vytvor_usecky(polygon):
+def vytvor_usecky(objekty):
     p_u = []
-    posledni_souradnice = len(polygon) - 1
-    for idx, point in enumerate(polygon):
-        if idx == posledni_souradnice:
-            p_u.append((point, polygon[0]))
-        else:
-            p_u.append((point, polygon[idx + 1]))
+    for polygon in objekty:
+        posledni_souradnice = len(polygon) - 1
+        for idx, point in enumerate(polygon):
+            if idx == posledni_souradnice:
+                p_u.append((point, polygon[0]))
+            else:
+                p_u.append((point, polygon[idx + 1]))
     return p_u
 
         # muj_prusecik = najdi_prusecik(n_u, p_u)
@@ -53,38 +51,7 @@ def najdi_prusecik(n_u, p_u):
         return (x, y)
 
 
-
-for polygon in objekty:
-    moje_usecky_v_polygonu = vytvor_usecky(polygon)
-    pruseciky = []
-    vysledne_body = []
-
-    for p_u in moje_usecky_v_polygonu:
-        p_u = (p_u[0][0], p_u[0][1], p_u[1][0], p_u[1][1])
-        pruseciky.append(najdi_prusecik(n_u, p_u))
-
-    if len(pruseciky) > 0:
-        ty_spravne_body_polygonu = pridej_body_polygonu_v_polorovine(objekty, polygon, point)
-        # toto mozna spojit jinak
-        vysledne_body.append([pruseciky, ty_spravne_body_polygonu])
-
-
-def pridej_body_polygonu_v_polorovine(objekty, polygon, point):
-    c = - ((n_u[3] - n_u[1]) * n_u[0] + (n_u[0] - n_u[2]) * n_u[1])
-    for polygon in objekty:
-        gut_body = []
-        for point in polygon:
-            bodik = [point[0], point[1]]
-            gut_body = []
-            c = - ((n_u[3] - n_u[1]) * n_u[0] + (n_u[0] - n_u[2]) * n_u[1])
-            x = ((n_u[3] - n_u[1]) * bodik[0] + (n_u[0] - n_u[2]) * bodik[1] + c)
-            if (c > 0) and (x > 0) or (c < 0) and (x < 0):
-                gut_body.append(bodik)
-            else:
-                continue
-    return gut_body
-
-moje_hrkhrk = vytvor_usecky(polygon)
+moje_hrkhrk = vytvor_usecky(objekty)
 # print(moje_hrkhrk)
 pruseciky = []
 for p_u in moje_hrkhrk:
